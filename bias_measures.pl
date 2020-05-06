cdc77119b2a9e0f9d407ebdb039f07907ffa6441
Jack Taylor <tayloj22@tcnj.edu>
2020-05-06 16:50:24 -0400

    File handling and extraction of necessary data complete, also updated format of mousecut.txt and included message about that change in our readme

--- bias_measures.pl
+++ bias_measures.pl
@@ -4,3 +4,67 @@
 
 use strict;
 use warnings;
+
+# Ask the user for the filename of our DNA sequence
+print "Please enter the filename of the FASTA format DNA sequence.\n";
+my $filename = <STDIN>;
+chomp $filename;
+
+# Open the file, or kill the program upon failure
+open(FH, '<', $filename) or die $!;
+
+# Remove FASTA header and store the remaining text into a string
+my $fastaHeader = <FH>;
+my $seq = "";
+while (<FH>) {
+    $seq .= <FH>;
+    chomp $seq;
+}
+# Close the file
+close(FH);
+
+# Convert the DNA sequence into a protein-coding sequence
+$seq =~ tr/T/U/;
+print ("The protein coding sequence is:$seq\n");
+# Now that the sequence is ready, we parse the codon usage table
+# Ask the user for the filename of our codon usage table
+print "Please enter the filename of the Codon Usage Table of a reference genome.\n";
+$filename = <STDIN>;
+chomp $filename;
+
+# Open the file, or kill the program upon failure
+open(FH, '<', $filename) or die $!;
+
+# Extract only the triplets and the frequencies from the table
+my @words;
+my @codons;
+my @freqs;
+my $count = 0;
+while (<FH>) {
+    chomp;
+    @words = split(' ');
+    $count = 0;
+    foreach my $word (@words) {
+        if ($count % 2 == 0) {
+            push(@codons, $word);
+        }
+        else {
+            push(@freqs, $word);
+        }
+        $count++;
+    }
+}
+# Close the file
+close(FH);
+
+foreach my $freq (@freqs) {
+    # Uses regex for finding text matching format: (some number) and deleting it
+    $freq =~ s/\(.*\)//;
+}
+
+print "The respective codons and their frequencies (per thousand):\n";
+for (my $i = 0; $i < 64; $i++) {
+    print "($codons[$i], $freqs[$i])\n";
+}
+
+
--- readme.txt
+++ readme.txt
@@ -5,3 +5,5 @@ https://www.ncbi.nlm.nih.gov/nuccore/NC_000071.6?report=fasta&from=142903115&to=
 Our mouse Codon Usage Table example:
 Mus musculus [gbrod]: 53036 CDS's (24533776 codons)
 https://www.kazusa.or.jp/codon/cgi-bin/showcodon.cgi?species=10090
+
+Please note that if any spaces occur between the frequency and count within the codon usage table, our program will treat them as separate tokens and misfile information.  This may occur if the count is smaller than the rest, and must be fixed before our program can properly parse it.
